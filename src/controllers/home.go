5a701c37bc67f1b7526eec1d927b3fbd58b81e77
Leonardo <leonardotandelicw@outlook.com>
2022-04-19 01:36:07 -0300

    feature: Primeiro nome na página inicial

--- src/controllers/home.go
+++ src/controllers/home.go
@@ -5,19 +5,22 @@ import (
 	"app/src/modelos"
 	"app/src/utils"
 	"net/http"
+	"strings"
 )
 
 //Carrega página inicial
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	//função para inserir dados dos cookies armazenados durante o login
 	cookies, _ := cookies.InserirDadosNaPagina(r)
+	primeiroNome := strings.Split(cookies.Nome, " ")
 
 	utils.ExecutarTemplate(w, "home.html", struct {
-		Cookies modelos.PageCookies
-		Pagina  string
+		Cookies      modelos.PageCookies
+		Pagina       string
+		PrimeiroNome string
 	}{
-
-		Cookies: cookies,
-		Pagina:  "Mapa Operações",
+		PrimeiroNome: primeiroNome[0],
+		Cookies:      cookies,
+		Pagina:       "Mapa Operações",
 	})
 }
