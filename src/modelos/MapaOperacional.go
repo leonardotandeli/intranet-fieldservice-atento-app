f54256b4e7bea47010c2ca52a246bb6ae3ba7257
Leonardo <leonardotandelicw@outlook.com>
2022-07-04 00:05:03 -0300

    fix: ajuste struct de Sites

--- src/modelos/MapaOperacional.go
+++ src/modelos/MapaOperacional.go
@@ -27,6 +27,9 @@ type Cliente struct {
 type Site struct {
 	IDSITE     uint64 `json:"idsite,omitempty"`
 	NOME       string `json:"nome,omitempty"`
+	SIGLA      string `json:"sigla,omitempty"`
+	UF         string `json:"uf,omitempty"`
+	ENDERECO   string `json:"endereco,omitempty"`
 	ID_USUARIO string `json:"id_usuario,omitempty"`
 }
 
